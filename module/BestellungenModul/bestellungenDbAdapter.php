9c11fab3568daf6b5318279c2b09d7ec0cfee9d2
Alexander Burcev <Stygian@Tio>
2016-07-26 13:46:41 +0200

    added bestelllung functionality

--- module/BestellungenModul/bestellungenDbAdapter.php
+++ module/BestellungenModul/bestellungenDbAdapter.php
@@ -14,7 +14,7 @@ class bestellungDbAdapter extends baseDbAdapter
     
     function getAllBestellungen()
     {
-        $kompnenetenList = [];
+        $bestellungenList = [];
         $query = "SELECT * FROM bestellungen";
         $results = $this->execSQL($query);
         foreach ($results as $result)
@@ -24,51 +24,44 @@ class bestellungDbAdapter extends baseDbAdapter
             $best->setB_Einkaufsdatum($result["b_einkaufsdatum"]);
             $best->setB_Kaufbeleg($result["b_kaufbeleg"]);
             
-            $kompnenetenList[] = $best;
+            $bestellungenList[] = $best;
         }
-        return $kompnenetenList;
+        return $bestellungenList;
     }
     
-    function getKompneneteById($komponente)
+    function getBestellungById($bestellung)
     {
-        $query = "SELECT * FROM komponenten WHERE k_id = " . $komponente->getK_ID();
+        $query = "SELECT * FROM bestellungen WHERE b_id = " . $bestellung->getB_ID();
         $results = $this->execSQL($query); 
         $result = $results[0];
-        $komp = new komponente();
-        $komp->setB_ID($result["b_id"]);
-        $komp->setL_ID($result["l_id"]);
-        $komp->setR_ID($result["r_id"]);
-        $komp->setK_Notitz($result["k_notitz"]);
-        $komp->setK_Geweahrleistungen($result["k_gewaehrleistungen"]);
-        $komp->setK_Hersteller($result["k_hersteller"]);
-        $komp->getKAR_ID($result["kar_id"]);
-        return $komp;
+        $best = new bestellung(); 
+        $best->setB_ID($result["b_id"]);
+        $best->setB_Einkaufsdatum($result["b_einkaufsdatum"]);
+        $best->setB_Kaufbeleg($result["b_kaufbeleg"]);
+            
+        return $best;
     }
     
-    function deleteKomponenteById($komponente)
+    function deleteBestellungById($bestellung)
     {
-        $this->delete("kompnonenten", "k_id = " . $komponente->getK_ID());
+        $this->delete("bestellungen", "b_id = " . $bestellung->getB_ID());
     }
         
         
-    function saveKomponente($kompnenete)
+    function saveBEstellung($bestellung)
     {
-        $ID = $kompnenete->getK_ID();
-        $select = "SELECT * FROM komponenten WHERE k_id = " . $ID;
+        $ID = $bestellung->getB_ID();
+        $select = "SELECT * FROM bestellungen WHERE b_id = " . $ID;
         $results = $this->execSQL($select);
-        $parameters[] = $kompnenete->getR_ID();
-        $parameters[] = $kompnenete->getL_ID;
-        $parameters[] = $kompnenete->getB_ID();
-        $parameters[] = $kompnenete->getK_Geweahrleistungen();
-        $parameters[] = $kompnenete->getK_Hersteller();
-        $parameters[] = $kompnenete->getKAR_ID();
+        $parameters[] = $bestellung->getB_Einkaufsdatum_ID();
+        $parameters[] = $bestellung->getB_Kaufbeleg();
         if(count($results) != 0)
         {           
-            $this->update("komponenten", $parameters, "kar_id = " . $ID);
+            $this->update("bestellungen", $parameters, "kar_id = " . $ID);
         }
         else
         {
-            $this->insert("komponenten", $parameters);
+            $this->insert("bestellungen", $parameters);
         }
     }
 }
