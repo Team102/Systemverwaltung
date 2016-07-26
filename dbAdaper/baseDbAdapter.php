937569b9a2c578fc4b697a341c94db5f879be76e
FadiKoch <fadi.koch@osf-global.com>
2016-07-26 08:36:03 +0200

    Update baseDbAdapter.php

--- dbAdaper/baseDbAdapter.php
+++ dbAdaper/baseDbAdapter.php
@@ -12,7 +12,7 @@ class baseDbAdapter
 {
     
     private $dbConnection;
-    private $serverUrl = "http://localhost/TEAM102";
+    private $serverUrl = "http://localhost.TEAM102";
     private $error;
     private $user;
 
